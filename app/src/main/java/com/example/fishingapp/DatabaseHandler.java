2c0172b6e98a055182e9f2c154e4b5ab54fb82af
alena <alena.fisher@enmu.edu>
2024-09-30 08:48:53 -0600

    Adding comments to the SQLHandler.java file and the DatabaseHandler.java file

--- app/src/main/java/com/example/fishingapp/DatabaseHandler.java
+++ app/src/main/java/com/example/fishingapp/DatabaseHandler.java
@@ -1,32 +1,57 @@
+/**
+ * @author  Alena Fisher
+ * Created on 9/28/2024
+ * Last updated on 9/30/2024
+ */
+
 package com.example.fishingapp;
-import android.annotation.SuppressLint;
-import android.content.ContentValues;
+// Importing necessary libraries
 import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-//import com.example.fishingapp.Model.ListModel; !!!!!!!!!!!!!!!
-import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseHandler extends SQLHandler {
+    /**
+     * This file is responsible for the addition, updating, and removal of users
+     * from the database
+     * @param context
+     */
+
     public DatabaseHandler(Context context) {
+        /**
+         * This method creates a new instance of the database
+         */
         super(context);
     }
 
     public void insertUser(User user) {
+        /**
+         * This method is responsible for handling the user that is passed
+         * as a parameter and placing it in the database
+         *
+         * @param The user to be placed into the database
+         */
         super.insertUser(user);
     }
 
     public List<User> getAllUsers() {
+        /**
+         * This method is responsible for retrieving the users from the database
+         */
         return super.getAllUsers();
     }
 
     public void updateUser(User user) {
+        /**
+         * This method is responsible for updating the user
+         * in the database
+         */
         super.updateUser(user);
     }
 
     public void deleteUser(int id) {
+        /**
+         * This method is responsible for deleting a user from the database
+         */
         super.deleteUser(id);
     }
 
