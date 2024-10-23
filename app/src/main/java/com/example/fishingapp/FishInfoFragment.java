6fbba992c56a746598e1989271fd1f1506e0c4d0
devonalonzo <DevonAlonzo72@gmail.com>
2024-10-22 18:59:23 -0600

    empty login interface

--- app/src/main/java/com/example/fishingapp/FishInfoFragment.java
+++ app/src/main/java/com/example/fishingapp/FishInfoFragment.java
@@ -14,6 +14,6 @@ public class FishInfoFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        return inflater.inflate(R.layout.fragment_fishinfo, container, false);
     }
 }
