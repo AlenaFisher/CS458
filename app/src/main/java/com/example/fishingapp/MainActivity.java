b06a83d44d234e51c5473ffe9430a5ea6a216d0e
devonalonzo <DevonAlonzo72@gmail.com>
2024-12-03 19:09:58 -0700

    added catch log

--- app/src/main/java/com/example/fishingapp/MainActivity.java
+++ app/src/main/java/com/example/fishingapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
@@ -45,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
         fakeUserDataBase.add(DAlonzo);
         fakeUserDataBase.add(AFisher);
         fakeUserDataBase.add(BGingrich);
+
+
 
     }
 
