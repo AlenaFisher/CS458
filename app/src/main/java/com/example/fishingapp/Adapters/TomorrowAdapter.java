90ad5e15347c297bf7abfb228e105b1de717bdca
alena <alena.fisher@enmu.edu>
2024-12-07 11:46:06 -0700

    Adding documentation, adjusting the profile appearance, adding a home button to the map.

--- app/src/main/java/com/example/fishingapp/Adapters/TomorrowAdapter.java
+++ app/src/main/java/com/example/fishingapp/Adapters/TomorrowAdapter.java
@@ -1,3 +1,13 @@
+/**
+ * @author Alena Fisher
+ * 10/27/2024
+ * Lst updated: 12/07/2024
+ * This adapter class is responsible for binding the weather forecast data for the next 7 days
+ * to the view in a RecyclerView. IT takes a list of TomorrowDomain obkects, which contain
+ * information about the day of the week, weather condition, high and low temperatures,
+ * and an image representing the weather condition. Glide is used to dynamically load the weather images.
+ */
+
 package com.example.fishingapp.Adapters;
 
 import android.content.Context;
@@ -6,56 +16,97 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.bumptech.glide.Glide;
-import com.example.fishingapp.Domains.Hourly;
 import com.example.fishingapp.Domains.TomorrowDomain;
 import com.example.fishingapp.R;
-
 import java.util.ArrayList;
 
 public class TomorrowAdapter extends RecyclerView.Adapter<TomorrowAdapter.ViewHolder> {
+    // List of TomorrowDomain objects representing the weather forecast for the next 7 days
     ArrayList<TomorrowDomain> items;
     Context context;
 
+    /**
+     * Constructor for TomorrowAdapter.
+     * Initializes the adapter with a list of TomorrowDomain objects
+     * @param items A list of TomorrowDomain objects representing weather forecasts.
+     */
     public TomorrowAdapter(ArrayList<TomorrowDomain> items) {
         this.items = items;
     }
 
+    /**
+     * Called when a new ViewHolder is created. It inflates the layout for each item in the RecyclerView.
+     * @param parent The ViewGroup into which the new View will be added after it is bound to
+     *               an adapter position.
+     * @param viewType The view type of the new View.
+     * @return a new ViewHolder instance.
+     */
     @NonNull
     @Override
     public TomorrowAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // inflate the layout for individual weather forecast item
         View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_tomorrow, parent, false);
+
+        // Get context to use for loading resources
         context = parent.getContext();
+
+        // Return a new ViewHolder with the inflated view
         return new ViewHolder(inflate);
     }
 
+    /**
+     * Bind the weather data for a specific day to the ViewHolder.
+     * Sets the text for the day, status, high and low temperatures, and loads the weather image.
+     * @param holder The ViewHolder which should be updated to represent the contents of the
+     *        item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
     public void onBindViewHolder(@NonNull TomorrowAdapter.ViewHolder holder, int position) {
+        // Set the day, weather status, high and low temperatures for the current position
         holder.dayTxt.setText(items.get(position).getDay());
         holder.statusTxt.setText(items.get(position).getStatus());
         holder.highTxt.setText(items.get(position).getHighTemp());
         holder.lowTxt.setText(items.get(position).getLowTemp());
 
+        // Dynamically load the weather image using Glide
         int drawableResourceId = holder.itemView.getResources().getIdentifier(items.get(position).getImageRes(), "drawable", holder.itemView.getContext().getPackageName());
         Glide.with(context).load(drawableResourceId).into(holder.image);
     }
 
+    /**
+     * Returns the total number of items in the RecyclerView.
+     * @return The number of items in the list
+     */
     @Override
     public int getItemCount() {
         return items.size();
     }
 
+    /**
+     * ViewHolder class that holds the view for each individual item in the RecyclerView.
+     * Each ViewHolder represents one day's weather forecast.
+     */
     public class ViewHolder extends RecyclerView.ViewHolder {
-
+        // TextViews for displaying day, status, high and low temperatures
         TextView dayTxt, statusTxt, lowTxt, highTxt;
 
+        // ImageView for displaying the weather image
         ImageView image;
+
+        /**
+         * Constructor for ViewHolder.
+         * Initializes the views that will display the weather data for a single day.
+         *
+         * @param itemView The view representing a single item in the RecyclerView.
+         */
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            // Initialize the views
             dayTxt = itemView.findViewById(R.id.dayTxt);
             statusTxt = itemView.findViewById(R.id.statusTxt);
             lowTxt = itemView.findViewById(R.id.lowTxt);
--- app/src/main/java/com/example/fishingapp/WeatherFragment.java
+++ app/src/main/java/com/example/fishingapp/WeatherFragment.java
@@ -1,7 +1,14 @@
+/**
+ * @author Alena Fisher
+ * 10/27/2024
+ * Last updated: 12/07/2024
+ * This fragment displays weather information for the user, including a list of hourly forecasts.
+ * It also provides a button to navigate to a detailed weather view for the next 7 days.
+ */
+
 package com.example.fishingapp;
 
 import android.view.View;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -11,50 +18,80 @@ import android.content.Intent;
 import android.widget.TextView;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.fishingapp.Activity.TomorrowActivity;
 import com.example.fishingapp.Adapters.HourlyAdapter;
 import com.example.fishingapp.Domains.Hourly;
-
 import java.util.ArrayList;
 
 public class WeatherFragment extends Fragment {
+    // RecyclerViews for displaying hourly data
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapterHourly;
 
+    /**
+     * Called when the fragment's view is created. It sets up the RecyclerView and button listeners.
+     *
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to.  The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return The root view for the fragment.
+     */
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.weather_main, container, false);
 
+        // Initialize the RecyclerView for displaying hourly weather.
         initRecyclerView(view);
+
+        // Set up the onClickListener to navigate to a detailed weather activity
         setNextDaysButtonListener(view);
         return view;
     }
 
+    /**
+     * Initializes the RecyclerView and sets up the hourly weather data to be displayed.
+     *
+     * @param view The root view of the fragment, used to find the RecyclerView.
+     */
     private void initRecyclerView(View view) {
+        // Creating a list of hourly weather data
         ArrayList<Hourly> items = new ArrayList<>();
 
-        items.add(new Hourly("2 pm", 83, "sunny"));
-        items.add(new Hourly("3 pm", 84, "sunny"));
-        items.add(new Hourly("4 pm", 84, "sunny"));
-        items.add(new Hourly("5 pm", 82, "sunny"));
-        items.add(new Hourly("6 pm", 75, "sunny"));
+        // Adding sample weather items to the list
+        items.add(new Hourly("2 pm", 42, "cloudy"));
+        items.add(new Hourly("3 pm", 42, "cloudy"));
+        items.add(new Hourly("4 pm", 40, "cloudy"));
+        items.add(new Hourly("5 am", 37, "rainy"));
+        items.add(new Hourly("6 am", 34, "rainy"));
 
+        // Find the RecyclerView by ID
         recyclerView = view.findViewById(R.id.view1);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
 
+        // Set the adapter to display the hourly weather data
         adapterHourly = new HourlyAdapter(items);
         recyclerView.setAdapter(adapterHourly);
     }
 
+    /**
+     * Sets up a listener for the "Next 7 Days" button to navigate to be detailed weather activity.
+     *
+     * @param view The root view of the fragment, used to find the button.
+     */
     private void setNextDaysButtonListener(View view) {
+        // Find the "Next 7 Days" button and set a click listener to navigate to TomorrowActivity
         TextView next7daysBtn = view.findViewById(R.id.nextBtn);
         next7daysBtn.setOnClickListener(v -> {
+            // Create an intent to start TomorrowActivity and navigate to the next 7 days forecast
             Intent intent = new Intent(getActivity(), TomorrowActivity.class);
-            startActivity(intent);
+            startActivity(intent); // Start the activity
         });
     }
 }
